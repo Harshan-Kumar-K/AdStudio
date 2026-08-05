cb3badaf98c354f8960658518c6bac2d9d182c4d
chaitanya-427 <kadalichaitanya.nagasatyacharan@cognizant.com>
2026-08-06 00:20:09 +0530

    Backend : partially succeded in swagger fixing for IAM

--- Backend/apiGateway/src/main/java/com/cts/apigateway/config/SecurityConfig.java
+++ Backend/apiGateway/src/main/java/com/cts/apigateway/config/SecurityConfig.java
@@ -40,7 +40,8 @@ public class SecurityConfig {
                 "http://localhost:5174",
                 "http://localhost:5175",
                 "http://localhost:5176",
-                "http://localhost:5177"
+                "http://localhost:5177",
+                "http://localhost:9090" // for swagger-ui.html access
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
